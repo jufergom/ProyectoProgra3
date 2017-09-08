99150d3173e05b5681be580bc03679aa3f2ad626
DarkJhair <jhairalberto6018@gmail.com>
2017-09-08 09:54:42 -0600

    Animacion Botones y algo de instruccion

--- main.cpp
+++ main.cpp
@@ -10,6 +10,16 @@ int main()
 
     BITMAP *fondo = load_bitmap("Fondo.bmp", NULL);
 
+    BITMAP *Jugar = load_bitmap("Jugar.bmp", NULL);
+
+    BITMAP *Instrucciones = load_bitmap("Instrucciones.bmp", NULL);
+
+    BITMAP *Scores = load_bitmap("Scores.bmp", NULL);
+
+    BITMAP *Salir = load_bitmap("Salir.bmp", NULL);
+
+    BITMAP *Instru = load_bitmap("Instru.bmp", NULL);
+
     BITMAP *cursor = load_bitmap("cursor.bmp", NULL);
 
     SAMPLE *musica = load_sample("musica.wav");
@@ -20,11 +30,35 @@ int main()
     while(!quit)
     {
         blit(fondo, buffer, 0, 0, 0, 0, 520, 550);
-        if(mouse_x > 205 && mouse_x < 318 && mouse_y > 444 && mouse_y < 492)
+        if(mouse_x > 190 && mouse_x < 331 && mouse_y > 239 && mouse_y < 283)
         {
+            blit(Jugar, buffer, 0, 0, 0, 0, 520, 550);
             if(mouse_b & 1)
                 quit = true;
         }
+
+        if(mouse_x > 105 && mouse_x < 424 && mouse_y > 305 && mouse_y < 348)
+        {
+            blit(Instrucciones, buffer, 0, 0, 0, 0, 520, 550);
+            if(mouse_b & 1)
+                blit(Instru, buffer, 0, 0, 0, 0, 520, 550);
+        }
+
+        if(mouse_x > 182 && mouse_x < 345 && mouse_y > 377 && mouse_y < 421)
+        {
+            blit(Scores, buffer, 0, 0, 0, 0, 520, 550);
+            if(mouse_b & 1)
+                quit = true;
+        }
+
+        if(mouse_x > 205 && mouse_x < 318 && mouse_y > 444 && mouse_y < 492)
+        {
+            blit(Salir, buffer, 0, 0, 0, 0, 520, 550);
+            if(mouse_b & 1)
+                quit = true;
+        }
+
+
         masked_blit(cursor, buffer, 0, 0, mouse_x, mouse_y, 13, 22);
         blit(buffer, screen, 0, 0, 0, 0, 520, 550);
     }
